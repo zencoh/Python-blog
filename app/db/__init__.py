9247b9fd088d458665a479829ec003a7a8cecd25
Cole Hartkopp <chsoccer4@outlook.com>
2023-07-26 16:40:38 -0500

    database moved within app for proper functionality

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,5 +1,5 @@
 from os import getenv
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
