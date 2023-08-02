00985806e602f6f4379c95b23441a05388717cf8
Cole Hartkopp <chsoccer4@outlook.com>
2023-08-02 11:42:33 -0500

    comment model and seeds

--- app/models/Post.py
+++ app/models/Post.py
@@ -11,5 +11,6 @@ class Post(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-
-user = relationship('User')
+    
+    user = relationship('User')
+    comments = relationship('Comment', cascade='all,delete')
