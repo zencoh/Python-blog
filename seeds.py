213aec6d00a2b2475bbd4efaede4801e4c521fd5
Cole Hartkopp <chsoccer4@outlook.com>
2023-08-02 12:03:59 -0500

    vote model, sqlalchemy func.count issues

--- seeds.py
+++ seeds.py
@@ -1,4 +1,4 @@
-from app.models import User, Post, Comment
+from app.models import User, Post, Comment, Vote
 from app.db import Session, Base, engine
 
 # drop and rebuild tables
@@ -31,6 +31,15 @@ db.add_all([
   Comment(comment_text='Aliquam erat volutpat. In congue.', user_id=2, post_id=1),
   Comment(comment_text='Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', user_id=2, post_id=3),
   Comment(comment_text='In hac habitasse platea dictumst.', user_id=3, post_id=3)
+])
+db.commit()
+
+db.add_all([
+  Vote(user_id=1, post_id=2),
+  Vote(user_id=1, post_id=4),
+  Vote(user_id=2, post_id=4),
+  Vote(user_id=3, post_id=4),
+  Vote(user_id=4, post_id=2)
 ])
 db.commit()
 
