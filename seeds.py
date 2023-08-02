61a731d0d13d3dbffcc2ea873d15edcea3ee1a82
Cole Hartkopp <chsoccer4@outlook.com>
2023-08-02 11:24:10 -0500

    user table created and seeded

--- seeds.py
+++ seeds.py
@@ -4,3 +4,17 @@ from app.db import Session, Base, engine
 # drop and rebuild tables
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
+
+db = Session()
+
+db.add_all([
+    User(username='zencoh', email='zencoh@email.com', password='password123'),
+    User(username='betaTester', email='btester@email.com', password='password123'),
+    User(username='testUser', email='tuser@email.com', password='password123'),
+    User(username='pixelPerformer', email='pperformer@email.com', password='password123'),
+    User(username='enigmaEvaluator', email='eevaluator@email.com', password='password123'),
+])
+
+db.commit()
+
+db.close()
